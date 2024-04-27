d30cc02032469c539a2d59a2d1661ce4338563b1
unknown <diffalchoel07@gmail.com>
2024-04-27 21:23:40 +0800

    Add

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prak3-13120220018"
+include(":app")
+ 
